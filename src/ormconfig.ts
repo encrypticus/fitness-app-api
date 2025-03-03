8a3c25521e96fbdeccb5c0678853166427054841
encrypticus <encrypticus@gmail.com>
2025-03-03 19:02:52 +0500

    ormconfig.ts added

---
+++ src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { CreateTableExercises1736086507264 } from '../src/lib/migrations/1736086507264-create_table_exercises';
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: parseInt(process.env.TYPEORM_PORT, 5432),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [CreateTableExercises1736086507264],
+  synchronize: false,
+  logging: true,
+});
