e353fc2969d674bf64283877266667b058f56697
encrypticus <encrypticus@gmail.com>
2025-03-08 21:06:31 +0500

    edit ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,11 @@ import { CreateTableExercises1736086507264 } from './lib/migrations/173608650726
 
 export default new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  host: process.env.TYPEORM_HOST,
+  port: parseInt(process.env.TYPEORM_PORT, 5432),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [CreateTableExercises1736086507264],
   synchronize: false,
