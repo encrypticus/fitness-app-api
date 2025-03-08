4a8ffed236be3631a4234cf7974a7a0a6cbb8e5c
encrypticus <encrypticus@gmail.com>
2025-03-08 18:41:30 +0500

    test github action

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('test github action');
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: ['http://localhost:8081', 'http://localhost:58944 '],
